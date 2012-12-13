dc1ce62507353c3d8a17ef3ebbb9a3d9e823fda0
Daniel Moniz <daniel.moniz@gmail.com>
2012-12-12 23:53:36 -0500

    Make command line executable with required username arg.

--- okcupid_profile_versioner.py
+++ okcupid_profile_versioner.py
@@ -1,23 +1,49 @@
+#!/usr/bin/env python
 import requests
 from bs4 import BeautifulSoup
+import sys
 
-file_name = "okcupid_profile{}".format('.html')
-file_path = "../okcupid/"
-r = requests.get('http://www.okcupid.com/profile/paragonrg')
-list_of_divs = ['essay_text_0', 'essay_text_1', 'essay_text_2', 'essay_text_3', 'essay_text_4', 'essay_text_5', 'essay_text_6', 'essay_text_7', 'essay_text_8', 'essay_text_9']
-#list_of_divs = ['essay_text_0']
-soup = BeautifulSoup(r.text)
+def main():
+    """First, parse command line arguments.
+    Arguments are: 
+        username
+    """
+
+    try:
+        username = sys.argv[1]
+    except IndexError:
+        #raise Exception("Must specify a username.")
+        print "Must specify a username. Type your username as the first argument."
+        print "Eg. $ python okcupid_profile_versioner.py my_username"
+        exit()
+
+    file_name = "okcupid_profile{}".format('.html')
+    file_path = "../okcupid/"
+    r = requests.get('http://www.okcupid.com/profile/{}'.format(username))
+    list_of_divs = ['essay_text_0', 'essay_text_1', 'essay_text_2', 
+        'essay_text_3', 'essay_text_4', 'essay_text_5', 'essay_text_6', 
+        'essay_text_7', 'essay_text_8', 'essay_text_9']
+    soup = BeautifulSoup(r.text)
+
+    html_output = ""
+    for div_id in list_of_divs:
+        # Get the HTML contents of the relevant div
+        try:
+            text = soup.select('div[id="{0}"]'.format(div_id))[0].contents
+        except IndexError:
+            print "No content found for username '{}'.".format(username)
+            exit()
+        for line in text:
+            html_output += str(line)
+            #f.write(str(line))
+        html_output += "<hr />"
+    f = open(file_path + file_name, 'w+')
+    f.write(html_output)
+    f.close()
+
+    print "-" * 80
+
+if __name__ == "__main__":
+    main()
 
 
-f = open(file_path + file_name, 'w+')
-for div_id in list_of_divs:
-    text = soup.select('div[id="{0}"]'.format(div_id))[0].contents
-    for line in text:
-        #print str(line)
-        f.write(str(line))
-    f.write("<hr />")
-    #print text
-    #print text
-f.close()
-
-print "-" * 80
