2b75e8770338db50a5abd7e91c6985ec6f8d92a2
Daniel Moniz <daniel.moniz@gmail.com>
2012-12-20 14:44:26 -0500

    Add ability to look at any profile by logging in.
    Fix #1: no longer breaks if users miss paragraph.

--- okcupid_profile_versioner.py
+++ okcupid_profile_versioner.py
@@ -17,13 +17,27 @@ def main():
         print "Eg. $ python okcupid_profile_versioner.py my_username"
         exit()
 
-    file_name = "okcupid_profile{}".format('.html')
+    file_name = "profile_okcupid_{}.html".format(username)
     file_path = "../okcupid/"
-    r = requests.get('http://www.okcupid.com/profile/{}'.format(username))
+
+    # First, try posting login credentials to the site.
+    # Note: Put in information for a dummy account!
+    login_info = {"username":"sandy_asaurus", "password":"sandyman", "p":"", "dest":""}
+    session = requests.session()
+    p = session.post("http://www.okcupid.com/login", data=login_info)
+    print p
+    authlink_cookie = p.cookies['authlink']
+    print p.cookies['authlink']
+    # OkCupid is not giving me the session cookie. Perhaps the session may not be needed?
+    logged_in_cookies = {"authlink":authlink_cookie}
+
+    r = session.get('http://www.okcupid.com/profile/{}'.format(username), cookies=logged_in_cookies)
+    
     list_of_divs = ['essay_text_0', 'essay_text_1', 'essay_text_2', 
         'essay_text_3', 'essay_text_4', 'essay_text_5', 'essay_text_6', 
         'essay_text_7', 'essay_text_8', 'essay_text_9']
     soup = BeautifulSoup(r.text)
+    print r.text
 
     html_output = ""
     for div_id in list_of_divs:
@@ -31,11 +45,9 @@ def main():
         try:
             text = soup.select('div[id="{0}"]'.format(div_id))[0].contents
         except IndexError:
-            print "No content found for username '{}'.".format(username)
-            exit()
+            print "No content found in {} for username '{}'.".format(div_id, username)
         for line in text:
             html_output += str(line)
-            #f.write(str(line))
         html_output += "<hr />"
     f = open(file_path + file_name, 'w+')
     f.write(html_output)
